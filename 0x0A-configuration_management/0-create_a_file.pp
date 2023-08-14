6d44f862bdd7883741b9d7b8f7cb177ff115d9ad
PEDA-ALPHA1 <daep100@gmail.com>
2023-08-14 08:18:46 -0700

    Create a file using Puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
-# Create a file at /tmp directory
+# create a file at /tmp directory
+# using the following requirements
 
 file { 'school':
-path => '/tmp/school',
-mode => '0744',
-owner => 'www-data',
-group => 'www-data',
-content => 'I love Puppet',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
